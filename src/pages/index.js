f5307617d6ea9a31a9d01993be33412ba6dc80ca
Zakaria Sumon <zsumonn@gmail.com>
2020-10-29 20:06:35 +0600

    2ad commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ const IndexPage = () => {
   return (
     <Layout>
       <Head title="Home" />
-      <h1>Hello.</h1>
+      <h1>Hello...</h1>
       <h2>
         I'm Zakaria Sumon, a full-stack developer living in beautyfull
         Bangladesh.
